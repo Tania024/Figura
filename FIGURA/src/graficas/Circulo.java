53a011238b3592a533ea9589d3263603eb63381a
Tania024 <tania01lojano@hotmail.com>
2021-06-22 23:26:59 -0500

    Figuras

---
+++ FIGURA/src/graficas/Circulo.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package graficas;
+
+import java.awt.Graphics;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Circulo {
+    public  void Dibujar (Graphics g){
+        g.drawOval(10,20,70,70 );
+    }   
+}
---
+++ FIGURA/src/graficas/Rectangulo.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package graficas;
+
+import java.awt.Graphics;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Rectangulo {
+        public  void Dibujar (Graphics g){
+        g.drawRect(10, 210, 70, 50);
+    }
+}
